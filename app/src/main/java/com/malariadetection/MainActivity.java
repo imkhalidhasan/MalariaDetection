6c7ba8aaa095517034b95d26d0da9bb688f62cf9
BMHasanuzzaman <bm.hasanuzzaman@northsouth.edu>
2020-11-19 22:18:39 +0600

    change of design

--- app/src/main/java/com/malariadetection/MainActivity.java
+++ app/src/main/java/com/malariadetection/MainActivity.java
@@ -26,26 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //phone = findViewById(R.id.profilePhone);
-        //fullName = findViewById(R.id.profileName);
-        //email    = findViewById(R.id.profileEmail);
-
-        fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
-
-        userId = fAuth.getCurrentUser().getUid();
-
-        DocumentReference documentReference = fStore.collection("users").document(userId);
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                phone.setText(documentSnapshot.getString("phone"));
-                fullName.setText(documentSnapshot.getString("fName"));
-                email.setText(documentSnapshot.getString("email"));
-            }
-        });
-
-
     }
 
     public void logout(View view) {
